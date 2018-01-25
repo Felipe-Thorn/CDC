32215bc3f4a8e92ebb89dda95bff78627447905a
Felipe-Thorn <lucasclashms@gmail.com>
2018-01-25 00:23:43 -0200

    New feature: use of libs

--- src/message.cmake
+++ src/message.cmake
@@ -1,4 +1,6 @@
 cmake_minimum_required (VERSION 3.0)
 
-#Verifica que o script foi aberto
-message(STATUS "Inside the script")
+#Warning message
+message(STATUS "The CMakeLists file in the src dir is becoming too complex...")
+
+
---
+++ src/message.cmake~
@@ -0,0 +1,4 @@
+cmake_minimum_required (VERSION 3.0)
+
+#Verifica que o script foi aberto
+message(STATUS "Inside the script")
