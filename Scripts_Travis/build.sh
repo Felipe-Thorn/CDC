6c6ad39a7d356df31269761d2a666d40ad9d39ac
Felipe Thorn <35688516+Felipe-Thorn@users.noreply.github.com>
2018-02-03 17:33:55 -0200

    New file: Script - build

---
+++ Scripts_Travis/build.sh
@@ -0,0 +1,6 @@
+- cd build
+- make
+- sudo make install
+- build-wrapper-linux-x86-64 --out-dir bw-output make   #The output is written in build
+- cd ..
+- sonar-scanner   #sonar-project.properties must access the correct output path
