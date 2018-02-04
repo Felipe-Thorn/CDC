050963e5727c34ca688110dab5275c5b91397d22
Felipe Thorn <35688516+Felipe-Thorn@users.noreply.github.com>
2018-02-03 22:02:58 -0200

    Update build.sh

--- Scripts_Travis/build.sh
+++ Scripts_Travis/build.sh
@@ -4,5 +4,5 @@ sudo make install
 build-wrapper-linux-x86-64 --out-dir bw-output make   #The output is written in build
 cd ..
 sonar-scanner   #sonar-project.properties must access the correct output path
-(cd .. && ls && cd .. && ls && cd .. && ls)
+(cd ../.. && ls)
 #cp -a  /home/travis/build/Felipe-Thorn/CDC/lib/ /home/travis/.casher
